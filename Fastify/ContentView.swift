165f289f1bfcae54be19eb891db7c790b8bcc24c
rizaadi <rizaadi890@gmail.com>
2024-03-20 08:16:38 +0700

    Initial Commit

---
+++ Fastify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fastify
+//
+//  Created by Riza Adi Kurniawan on 20/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fastify/FastifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastifyApp.swift
+//  Fastify
+//
+//  Created by Riza Adi Kurniawan on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FastifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
