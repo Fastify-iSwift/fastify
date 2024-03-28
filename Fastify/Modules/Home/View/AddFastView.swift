ad2056491ac3b292dbc18da92e4a81d1350f46df
rizaadi <rizaadi890@gmail.com>
2024-03-28 21:06:46 +0700

    feat: add fasting page

---
+++ Fastify/Modules/Home/View/AddFastView.swift
@@ -0,0 +1,67 @@
+//
+//  AddFastView.swift
+//  Fastify
+//
+//  Created by Riza Adi Kurniawan on 27/03/24.
+//
+
+import SwiftUI
+
+struct AddFast: View {
+    @State private var startTime = Date()
+    @State private var endTime = Date(timeIntervalSinceNow: (60 * 60))
+    
+    var totalTime: TimeInterval {
+        endTime.timeIntervalSince(startTime)
+    }
+    
+    var dateFormatter: DateComponentsFormatter {
+        let df = DateComponentsFormatter()
+        df.allowedUnits = [.hour,.minute,]
+        df.unitsStyle = .abbreviated
+        df.zeroFormattingBehavior = .pad
+        
+        
+        return df
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Total Time")
+                    .font(.headline)
+                    .padding()
+                
+                Text(dateFormatter.string(from: totalTime) ?? "")
+                    .font(.system(size: 48))
+                    .fontWeight(.semibold)
+                
+                DatePicker("Start", selection: $startTime, displayedComponents: .hourAndMinute)
+                    .fontWeight(.medium)
+                
+                DatePicker("End", selection: $endTime, displayedComponents: .hourAndMinute)
+                    .padding(.bottom, 20)
+                    .fontWeight(.medium)
+                
+                Button("Start Fasting") {
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(red: 0/255, green: 128/255, blue: 128/255))
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                Spacer()
+                
+            }
+            .padding()
+            .navigationTitle("Add Time")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
+    }
+}
+
+#Preview {
+    AddFast()
+}
